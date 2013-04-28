0ae049980384dbe8cab494b9cfb2f4d0385f6bc5
henzk <speidel@schnapptack.de>
2013-04-29 01:41:31 +0200

    fix MetaRuleset

--- fstcomp/composer/rulesets/MetaRuleset.java
+++ fstcomp/composer/rulesets/MetaRuleset.java
@@ -1,5 +1,6 @@
 package composer.rulesets;
 
+import composer.Configuration;
 import composer.rules.meta.ConstructorConcatenationMeta;
 import composer.rules.meta.ContractCompositionMeta;
 import composer.rules.meta.FieldOverridingMeta;
@@ -7,13 +8,14 @@ import composer.rules.meta.InvariantCompositionMeta;
 import composer.rules.meta.JavaMethodOverridingMeta;
 
 public class MetaRuleset extends JMLRuleset {
-	public MetaRuleset(String contractStyle) {
-		super(contractStyle);
-		setRule(new JavaMethodOverridingMeta());
+
+	@Override
+	public void configure(Configuration conf) {
+		super.configure(conf);
+		setRule(new JavaMethodOverridingMeta(metadataStore));
 		setRule(new InvariantCompositionMeta());
-		setRule(new ContractCompositionMeta(contractStyle));
+		setRule(new ContractCompositionMeta(conf.contract_style));
 		setRule(new ConstructorConcatenationMeta());
 		setRule(new FieldOverridingMeta());
-
 	}
 }
