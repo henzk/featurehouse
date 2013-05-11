6c72ad94a06316519f5e1682f1ce2435eead901d
henzk <speidel@schnapptack.de>
2013-05-11 16:39:20 +0200

    remove CRuntimeReplacement and JavaRuntimeReplacement (both clones of Replacement)

--- fstcomp/composer/rulesets/CVarEncRuleset.java
+++ fstcomp/composer/rulesets/CVarEncRuleset.java
@@ -5,14 +5,12 @@ import java.io.IOException;
 import composer.Configuration;
 import composer.rules.rtcomp.c.CRuntimeFeatureSelection;
 import composer.rules.rtcomp.c.CRuntimeFunctionRefinement;
-import composer.rules.rtcomp.c.CRuntimeReplacement;
 
 public class CVarEncRuleset extends VarEncRuleset {
 	
 	@Override
 	public void configure(Configuration conf) {
 		super.configure(conf);
-		setRule(new CRuntimeReplacement());
 		setRule(new CRuntimeFunctionRefinement());
 	}
 
